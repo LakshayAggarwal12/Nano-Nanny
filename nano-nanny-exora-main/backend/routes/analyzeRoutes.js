771e7341f23495fee864bbaa17663cdb3da1f079
LakshayAggarwal12 <lakshayagarwal2005@gmail.com>
2026-03-21 00:29:35 +0530

    extra-delete

--- nano-nanny-exora-main/backend/routes/analyzeRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { analyzePatientData } = require("../controllers/analyzeController");
-
-router.post("/", analyzePatientData);
-
-module.exports = router;
